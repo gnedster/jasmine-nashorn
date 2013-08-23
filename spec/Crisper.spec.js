83346bfafa54c12340600f70682f1ca2bb4312c1
pickledonion <onion@pickledonion.com>
2013-08-23 14:00:54 +0100

    runs with jjs. call execute()

---
+++ spec/Crisper.spec.js
@@ -0,0 +1,6 @@
+describe("Crisper", function() {
+    it("should be a constructor function", function() {
+        // expect(typeof window.Crisper).toBe("function");
+      expect(13).toBe(13);
+    });
+});
---
+++ spec/oh.spec.js
@@ -0,0 +1,5 @@
+describe("Crisper", function() {
+    it("should be a constructor function", function() {
+        expect(11).toBe("1111");
+    });
+});
