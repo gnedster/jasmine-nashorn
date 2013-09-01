9939a09f4d80bfba4b3e598b84fd44a8b0dfcd7f
pickledonion <onion@pickledonion.com>
2013-09-01 13:27:00 +0100

    change to work as component

--- spec/Crisper.spec.js
+++
@@ -1,6 +0,0 @@
-describe("Crisper", function() {
-    it("should be a constructor function", function() {
-        // expect(typeof window.Crisper).toBe("function");
-      expect(13).toBe(13);
-    });
-});
--- spec/oh.spec.js
+++
@@ -1,5 +0,0 @@
-describe("Crisper", function() {
-    it("should be a constructor function", function() {
-        expect(11).toBe("1111");
-    });
-});
